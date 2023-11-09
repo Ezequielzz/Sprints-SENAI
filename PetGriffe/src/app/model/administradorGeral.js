66d8af8902db43e74d0b35382bab5cd51a190604
Alberto-MeloL <albertomeloteco@gmail.com>
2023-11-09 13:18:25 -0300

    09/11/2023

--- PetGriffe/src/app/model/administradorGeral.js
+++ PetGriffe/src/app/model/administradorGeral.js
@@ -3,11 +3,11 @@ import Express from "express";
 
 const app = Express();
 
-app.get("/administrador", (req, res) =>{
+app.get("/administrador", (req, res, next) =>{
 
 });
 
 
 app.listen(7080, ()=>{
-    console.log("Servidor rodando em http:localhost:7080");
+    console.log("Servidor rodando em //http:localhost:7080");
 });
